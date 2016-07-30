bbb7379b20e23e13cbf53e93d1b2901a56be6441
sani <khursani8@gmail.com>
2016-07-31 01:11:37 +0800

    test

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -6,7 +6,7 @@ module.exports = {
 
   // MongoDB connection options
   mongo: {
-    uri: process.env.MONGOLAB_URI || 'mongodb://localhost/me-dev'
+    uri: process.env.MONGOLAB_BLUE_URI || 'mongodb://heroku_ptzb90lr:6vpampgkagjhtd4kdd07et550i@ds031975.mlab.com:31975/heroku_ptzb90lr' || 'mongodb://localhost/me-dev'
    // uri:'mongodb://khursani:1261995s@ds021915.mlab.com:21915/internapp'
   },
 
