e55b986dd4811b20252dc21ed176b7b291343ead
sani <khursani8@gmail.com>
2016-07-30 23:53:01 +0800

    heroku

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -15,7 +15,7 @@ module.exports = {
 
   // MongoDB connection options
   mongo: {
-    uri:  process.env.MONGODB_URI ||
+    uri:  process.env.MONGODB_URI || process.env.MONGOLAB_URI ||
           process.env.MONGOHQ_URL ||
           process.env.OPENSHIFT_MONGODB_DB_URL +
           process.env.OPENSHIFT_APP_NAME || 'mongodb://khursani:1261995s@ds021915.mlab.com:21915/internapp' ||
